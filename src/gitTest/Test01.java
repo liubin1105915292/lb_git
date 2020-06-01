406e6d03827b0eaf886d1cf5c63b50f75aa27491
liubin <1105915292@qq.com>
2020-06-01 19:25:13 +0800

    修改2提交

--- src/gitTest/Test01.java
+++ src/gitTest/Test01.java
@@ -4,5 +4,11 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println(6666);
 		System.out.println(9999789);
+		System.out.println(222);
+	}
+	
+	
+	public static void t1(){
+		System.out.println(1233);
 	}
 }
